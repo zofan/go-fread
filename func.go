a1818ac3dc1b57f7664af61f87b893865883273c
andrew <0x9171@gmail.com>
2021-03-20 14:56:02 +0300

    upd

--- func.go
+++ func.go
@@ -2,21 +2,19 @@ package fread
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"os"
 )
 
 func ReadJson(path string, i interface{}) error {
 	fh, err := os.OpenFile(path, os.O_RDONLY, 0666)
 	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+
 		return err
 	}
 	defer fh.Close()
 
-	raw, err := ioutil.ReadAll(fh)
-	if err != nil {
-		return err
-	}
-
-	return json.Unmarshal(raw, i)
+	return json.NewDecoder(fh).Decode(i)
 }
